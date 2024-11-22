6b60de722b802f2abd300a70ad33ccaf27d0c5b1
DanyBarrueto <dfbarrueto@hotmail.com>
2024-11-21 19:37:59 -0500

    V1.1.1
    devolucion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,13 @@ import { Router } from '@angular/router';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'centaurosbank';
-  constructor(private router: Router) {}
-
-  IniciarSesion() {
-    const url = this.router.serializeUrl(
-      this.router.createUrlTree(['/ingreso'])
-    );
-    window.open(url, '_blank');
+  constructor(private router:Router){}
+  
+  IniciarSesion(){
+    this.router.navigate(['/ingreso']);
   }
 }
