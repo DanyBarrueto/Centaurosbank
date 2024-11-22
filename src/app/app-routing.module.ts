8d945f80bb3e32a5d9b8bf61b367a73087a9ae97
DanyBarrueto <dfbarrueto@hotmail.com>
2024-11-21 20:18:34 -0500

    V1.5
    ya redirecciona a iniciar sesión
    
    Co-Authored-By: Owen Murcia <139822791+SebRage@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IngresoComponent } from './ingreso/ingreso.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', component:HomeComponent},
   {path: 'ingreso', component:IngresoComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IngresoComponent } from './ingreso/ingreso.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IngresoComponent
+    IngresoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
