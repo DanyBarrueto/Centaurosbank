24df50cc0b6c991db3ad64c63ff6ef49a71eff91
DanyBarrueto <dfbarrueto@hotmail.com>
2024-11-21 19:35:48 -0500

    V1.1
    si pero no ya se tiene que muestre algo pero no redirige

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'centaurosbank';
+  constructor(private router: Router) {}
+
+  IniciarSesion() {
+    const url = this.router.serializeUrl(
+      this.router.createUrlTree(['/ingreso'])
+    );
+    window.open(url, '_blank');
+  }
 }
---
+++ src/app/ingreso/ingreso.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso',
+  standalone: false,
+  
+  templateUrl: './ingreso.component.html',
+  styleUrl: './ingreso.component.css'
+})
+export class IngresoComponent {
+
+}
